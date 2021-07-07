86005aacd8e56d285c922d3d3d5f0c43f592a58a
Kyle Nance <nancekyle15@gmail.com>
2021-07-07 19:21:02 -0400

    doesnt run yet

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        message: "What would you like the title to be?" ,
+        input: ,
+    },
+    {
+        question: "Enter a project description." ,
+        choice: [] ,
+    },
+    {
+        question: "What was your motivation for this project? " ,
+        choice: []
+    },
+    {
+        question: "Why did you build this project?" ,
+        choice: []
+    },
+    {
+        question: "What problem does it solve?" ,
+        choice: []
+    },
+    {
+        question: "Would you like to add a table of contents" ,
+        choice: []
+    },
+    {
+        question: "What steps are required for installation of your project?" ,
+        choice: []
+    },
+    {
+        question: "Provide an example of the usage of your project?" ,
+        choice: []
+    },
+    {
+        question: "Who would you like to credit in this project?" ,
+        choice: []
+    },
+    {
+        question: "Are there any licences that your project uses?" ,
+        choice: []
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+  
+}
 // TODO: Create a function to initialize app
 function init() {}
 
